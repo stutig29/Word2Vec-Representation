fb2277a3c21127e99f24d94e3a556bf9837781d9
stutig29 <43212744+stutig29@users.noreply.github.com>
2019-08-13 22:28:29 +0530

    Ans2_gradcheck

--- q2_gradcheck.py
+++ q2_gradcheck.py
@@ -7,7 +7,6 @@ import random
 # First implement a gradient checker by filling in the following functions
 def gradcheck_naive(f, x):
     """ Gradient check for a function f.
-
     Arguments:
     f -- a function that takes a single argument and outputs the
          cost and its gradients
@@ -23,29 +22,30 @@ def gradcheck_naive(f, x):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
-
         # Try modifying x[ix] with h defined above to compute
         # numerical gradients. Make sure you call random.setstate(rndstate)
         # before calling f(x) each time. This will make it possible
         # to test cost functions with built in randomness later.
 
         ### YOUR CODE HERE:
-        raise NotImplementedError
+        random.setstate(rndstate)
+        fx1,grad1=f(x[ix]+h)
+        fx2,grad2=f(x[ix]-h)
+        numgrad=(fx1-fx2)/(2*h)
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (
-                grad[ix], numgrad)
+            print ("Gradient check failed.")
+            print ("First gradient error found at index %s" % str(ix))
+            print ("Your gradient: %f \t Numerical gradient: %f" % (
+                grad[ix], numgrad))
             return
 
         it.iternext() # Step to next dimension
 
-    print "Gradient check passed!"
-
+    print ("Gradient check passed!")
 
 def sanity_check():
     """
@@ -53,13 +53,12 @@ def sanity_check():
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print ("Running sanity checks...")
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
     gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
-
-
+    print ("")
+    
 def your_sanity_checks():
     """
     Use this space add any additional sanity checks by running:
@@ -67,12 +66,14 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    quad = lambda x: (np.sum(x ** 2), x * 2)
+    print ("Running your sanity checks...")
     ### YOUR CODE HERE
-    raise NotImplementedError
+    gradcheck_naive(quad, np.random.randn(5,6,7))
     ### END YOUR CODE
-
 
 if __name__ == "__main__":
     sanity_check()
     your_sanity_checks()
+    
+
