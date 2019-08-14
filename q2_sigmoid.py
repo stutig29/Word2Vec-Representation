714b122adf28672592ae8a5a69385bc655e58ce9
stutig29 <43212744+stutig29@users.noreply.github.com>
2019-08-14 16:25:19 +0530

    Update q2_sigmoid.py

--- q2_sigmoid.py
+++ q2_sigmoid.py
@@ -6,16 +6,14 @@ import numpy as np
 def sigmoid(x):
     """
     Compute the sigmoid function for the input here.
-
     Arguments:
     x -- A scalar or numpy array.
-
     Return:
     s -- sigmoid(x)
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s=1/(1 + np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -26,16 +24,14 @@ def sigmoid_grad(s):
     Compute the gradient for the sigmoid function here. Note that
     for this implementation, the input s should be the sigmoid
     function value of your original input x.
-
     Arguments:
     s -- A scalar or numpy array.
-
     Return:
     ds -- Your computed gradient.
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds= s*(1-s)
     ### END YOUR CODE
 
     return ds
@@ -46,21 +42,21 @@ def test_sigmoid_basic():
     Some simple tests to get you started.
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print ("Running basic tests...")
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
-    print f
+    print (f)
     f_ans = np.array([
         [0.73105858, 0.88079708],
         [0.26894142, 0.11920292]])
     assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
-    print g
+    print (g)
     g_ans = np.array([
         [0.19661193, 0.10499359],
         [0.19661193, 0.10499359]])
     assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
-    print "You should verify these results by hand!\n"
+    print ("You should verify these results by hand!\n")
 
 
 def test_sigmoid():
@@ -70,7 +66,7 @@ def test_sigmoid():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print ("Running your tests...")
     ### YOUR CODE HERE
     raise NotImplementedError
     ### END YOUR CODE
@@ -78,4 +74,4 @@ def test_sigmoid():
 
 if __name__ == "__main__":
     test_sigmoid_basic();
-    test_sigmoid()
+    #test_sigmoid()
