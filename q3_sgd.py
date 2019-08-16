82fd7eadc4c467e445f607c8783eee0d0124e0fe
stutig29 <43212744+stutig29@users.noreply.github.com>
2019-08-16 14:50:44 +0530

    Ans q3_sgd

--- q3_sgd.py
+++ q3_sgd.py
@@ -7,7 +7,7 @@ import glob
 import random
 import numpy as np
 import os.path as op
-import cPickle as pickle
+import pickle as pickle
 
 
 def load_saved_params():
@@ -39,9 +39,7 @@ def save_params(iter, params):
 def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
         PRINT_EVERY=10):
     """ Stochastic Gradient Descent
-
     Implement the stochastic gradient descent method in this function.
-
     Arguments:
     f -- the function to optimize, it should take a single
          argument and yield two outputs, a cost and the gradient
@@ -53,7 +51,6 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
                       if necessary. In the case of word2vec we will need to
                       normalize the word vectors to have unit length.
     PRINT_EVERY -- specifies how many iterations to output loss
-
     Return:
     x -- the parameter value after SGD finishes
     """
@@ -79,13 +76,15 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
     expcost = None
 
-    for iter in xrange(start_iter + 1, iterations + 1):
+    for iter in range(start_iter + 1, iterations + 1):
         # Don't forget to apply the postprocessing after every iteration!
         # You might want to print the progress every few iterations.
 
         cost = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+        cost, grad = f(x0)
+        x0 = x0 - (step * grad)
+        x=x0
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
@@ -93,34 +92,34 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
                 expcost = cost
             else:
                 expcost = .95 * expcost + .05 * cost
-            print "iter %d: %f" % (iter, expcost)
+            print ("iter %d: %f" % (iter, expcost))
 
         if iter % SAVE_PARAMS_EVERY == 0 and useSaved:
             save_params(iter, x)
 
         if iter % ANNEAL_EVERY == 0:
             step *= 0.5
-
+    
     return x
 
 
 def sanity_check():
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print ("Running sanity checks...")
     t1 = sgd(quad, 0.5, 0.01, 1000, PRINT_EVERY=100)
-    print "test 1 result:", t1
+    print ("test 1 result:", t1)
     assert abs(t1) <= 1e-6
 
     t2 = sgd(quad, 0.0, 0.01, 1000, PRINT_EVERY=100)
-    print "test 2 result:", t2
+    print ("test 2 result:", t2)
     assert abs(t2) <= 1e-6
 
     t3 = sgd(quad, -1.5, 0.01, 1000, PRINT_EVERY=100)
-    print "test 3 result:", t3
+    print ("test 3 result:", t3)
     assert abs(t3) <= 1e-6
 
-    print ""
+    print ("")
 
 
 def your_sanity_checks():
@@ -130,7 +129,7 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    print ("Running your sanity checks...")
     ### YOUR CODE HERE
     raise NotImplementedError
     ### END YOUR CODE
@@ -138,4 +137,4 @@ def your_sanity_checks():
 
 if __name__ == "__main__":
     sanity_check()
-    your_sanity_checks()
+    #your_sanity_checks()
